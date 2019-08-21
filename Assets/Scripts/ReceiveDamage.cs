02037778f5227336d8e182917f202e4aa88ef947
Helmi Amsani <48036579+helmiamsani@users.noreply.github.com>
2019-08-21 11:37:22 +1000

    Done!! xD

---
+++ Assets/Scripts/ReceiveDamage.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+
+public class ReceiveDamage : NetworkBehaviour
+{
+    [SerializeField]
+    private int maxHealth = 10;
+
+    [SyncVar]
+    private int currentHealth;
+
+    [SerializeField]
+    private string targetTag;
+
+    [SerializeField]
+    private int damageToTarget;
+
+    [SerializeField]
+    private bool destroyOnDeath;
+
+    private Vector2 initialPosition;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.currentHealth = this.maxHealth;
+        this.initialPosition = this.transform.position;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (collider.tag == this.targetTag)
+        {
+            this.TakeDamage(damageToTarget);
+
+            Destroy(collider.gameObject);
+        }
+    }
+
+    void TakeDamage(int amount)
+    {
+        if (this.isServer)
+        {
+            this.currentHealth -= amount;
+            if (this.currentHealth <= 0)
+            {
+                if (this.destroyOnDeath)
+                {
+                    Destroy(this.gameObject);
+                }
+                else
+                {
+                    this.currentHealth = this.maxHealth;
+                }
+            }
+        }
+    }
+
+    [ClientRpc]
+    void RpcRespawn()
+    {
+        this.transform.position = this.initialPosition;
+    }
+}
---
+++ Assets/Scripts/ShootBullet.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+
+public class ShootBullet : NetworkBehaviour
+{
+    [SerializeField]
+    private GameObject bulletPrefab;
+
+    [SerializeField]
+    private float bulletSpeed;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(this.isLocalPlayer && Input.GetKeyDown(KeyCode.Space))
+        {
+            this.CmdShoot();
+        }
+    }
+
+    void CmdShoot()
+    {
+        GameObject bullet = Instantiate(bulletPrefab, this.transform.position, Quaternion.identity);
+        bullet.GetComponent<Rigidbody2D>().velocity = Vector2.up * bulletSpeed;
+        NetworkServer.Spawn(bullet);
+        Destroy(bullet, 1.0f);
+    }
+}
