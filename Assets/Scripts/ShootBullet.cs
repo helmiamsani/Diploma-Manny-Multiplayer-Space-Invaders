9c6c251e20ba6f2255f96b68268ac0a43adf55dc
Helmi Amsani <48036579+helmiamsani@users.noreply.github.com>
2019-08-21 14:14:04 +1000

    Updated

--- Assets/Scripts/ShootBullet.cs
+++ Assets/Scripts/ShootBullet.cs
@@ -16,15 +16,25 @@ public class ShootBullet : NetworkBehaviour
     {
         if(this.isLocalPlayer && Input.GetKeyDown(KeyCode.Space))
         {
-            this.CmdShoot();
+            this.CmdShoot(transform.position);
         }
     }
 
-    void CmdShoot()
+    // Commands - Only run on Server
+    // Rpc(Remore Procedure Call) - Runs on Client but called from Server
+
+    [ClientRpc]
+    void RpcClientShot(Vector3 position)
     {
-        GameObject bullet = Instantiate(bulletPrefab, this.transform.position, Quaternion.identity);
-        bullet.GetComponent<Rigidbody2D>().velocity = Vector2.up * bulletSpeed;
-        NetworkServer.Spawn(bullet);
+        GameObject bullet = Instantiate(bulletPrefab, position, Quaternion.identity);
+        Rigidbody2D rigid = bullet.GetComponent<Rigidbody2D>();
+        rigid.velocity = Vector2.up * bulletSpeed;
         Destroy(bullet, 1.0f);
+    }
+
+    [Command]
+    void CmdShoot(Vector3 position)
+    {
+        RpcClientShot(position);
     }
 }
